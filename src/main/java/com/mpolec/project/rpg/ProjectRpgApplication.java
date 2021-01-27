de550052b0badb2cbfc19317113db526acd7ef9f
Mateonidas <teus.polec@gmail.com>
2021-01-27 18:58:27 +0100

    Initial commit

---
+++ src/main/java/com/mpolec/project/rpg/ProjectRpgApplication.java
@@ -0,0 +1,13 @@
+package com.mpolec.project.rpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRpgApplication.class, args);
+	}
+
+}
